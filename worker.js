cab6eaffea847561128e9b3ba48f874d5db81449
Dylan O'Driscoll <dylan.odriscoll@gmail.com>
2018-03-29 18:03:52 +0100

    Cleanup

--- worker.js
+++ worker.js
@@ -11,20 +11,11 @@ onmessage = function(event) {
 width = 1200
 height = 660
 
-//   var simulation = d3.forceSimulation(nodes)
-//         .force("link", d3.forceLink(links).distance(null).strength(function(d) { return d.weight/20 }))
-//         .force("charge", d3.forceManyBody().strength(-80).distanceMax(100))
-//         .force("center", d3.forceCenter(width / 2, height / 2))
-//         .force('collision', d3.forceCollide().radius(function(d) { return d.radius*2 }))
-//         .alphaMin(0.001)
-
     var simulation = d3.forceSimulation(nodes)
         .force("charge", d3.forceManyBody().strength(-80).distanceMax(100))
         .force("link", d3.forceLink(links).distance(20).strength(function(d) { return d.weight/20 }))
         .force("center", d3.forceCenter(width / 2, height / 2))
-        //.force("x", d3.forceX(width / 2))
         .force("y", d3.forceY(height / 2))
-        //.force('collision', d3.forceCollide().radius(function(d) { return d.radius*2 }))
         .stop();
 
 
