68e7f33eaa10d0c5cdde07f188350fbb09f46c68
Dylan O'Driscoll <dylan.odriscoll@gmail.com>
2024-04-27 11:27:42 +0100

    add episode rating

--- public/episodePicker/tv_show_episodes.js
+++ public/episodePicker/tv_show_episodes.js
@@ -71,13 +71,14 @@ async function pickAnEpisode() {
 
   const episode = await getEpisode(tvShow.id, season.season_number, episodeNumber)
 
-
   document.getElementById('poster').src = `https://image.tmdb.org/t/p/original/${season.poster_path}`
   document.getElementById('still').src = `https://image.tmdb.org/t/p/original/${episode.still_path}`
   document.getElementById('title').innerHTML = 'Why not try...'
   document.getElementById("episodeTitle").innerHTML = episode.name
   document.getElementById("episodeNumber").innerHTML = `Season ${season.season_number} Episode ${episodeNumber}`
   document.getElementById("releaseDate").innerHTML = `First aired: ${new Date(episode.air_date).toLocaleString().split(',')[0]}`
+  document.getElementById("rating").innerHTML = `Average rating: ${episode.vote_average} from ${episode.vote_count} user(s)`
+
   document.getElementById("description").innerHTML = episode.overview
   document.getElementById("resultsBox").style.display = 'block'
   document.getElementById("searchAgain").innerHTML = "Don't like it? Try Again"
